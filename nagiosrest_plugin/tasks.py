838444f6dd5829ba5624a67d5f52d8750339b0c7
MichalWilczynski <42137260+MichalWilczynski@users.noreply.github.com>
2018-12-17 16:40:21 +0100

    Update tasks.py

--- nagiosrest_plugin/tasks.py
+++ nagiosrest_plugin/tasks.py
@@ -152,7 +152,8 @@ def _make_call(ctx, request_method, url, data, operation_inputs):
         )
     elif result.status_code >= 400:
         raise RecoverableError(
-            'Parameters passed to server were incorrect. '
+            'Parameters passed to server were incorrect,'
+            'or Nagios is still starting. '
             'Call was to {url}, and '
             'response was {code}: {details}'.format(
                 url=url,
